b3c3b24b58b2a05a016ec6415293b49a3750df29
BlueOfficer <jdhbatman@gmail.com>
2025-05-16 18:39:52 +0000

    More Setup

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 class Main{
@@ -8,18 +9,37 @@ class Main{
         System.out.println("Welcome to bad mine sweeper");
         System.out.print("Please enter your number of cols here: ");
 
-        Scanner UserInput = new Scanner(System.in);
+        int Size = 10;
+
+        MineCell[][] Map = new MineCell[Size][Size];
+
+
+        Random rand = new Random();
+
+        for (int Row = 0; Row < Map.length; Row++) {
+            for (int Col = 0; Col < Map[Row].length; Col++) {
+                Map[Row][Col] = new MineCell(rand.nextBoolean());
+            }
+        }
+
+        
 
         
 
 
     }
 
+    pri
+
 }
 
 class MineCell{
 
+    //Shows Wethier its live ie clicking it will end the game
     Boolean IsActive;
+
+    //is used to show weither it should be show on the map
+    Boolean IsFound;
 
     MineCell(Boolean isActive){
         IsActive = isActive;
