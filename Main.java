4c7dd3cd1f55a3bf63067a26b13330591627526c
BlueOfficer <jdhbatman@gmail.com>
2025-05-16 18:33:26 +0000

    asd

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Main{
+
+    MineCell[][] Map;
+
+    public static void main(String[] args){
+        System.out.println("Welcome to bad mine sweeper");
+        System.out.print("Please enter your number of cols here: ");
+
+        Scanner UserInput = new Scanner(System.in);
+
+        
+
+
+    }
+
+}
+
+class MineCell{
+
+    Boolean IsActive;
+
+    MineCell(Boolean isActive){
+        IsActive = isActive;
+    }
+
+}
